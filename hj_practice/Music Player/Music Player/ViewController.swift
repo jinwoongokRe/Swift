8b5a16b7f1cd4a73680991aaa72b952a2ea6c802
huskycat1202 <annie991202@naver.com>
2022-05-06 23:39:44 +0900

    mod: 2-4(IBAction)

--- hj_practice/Music Player/Music Player/ViewController.swift
+++ hj_practice/Music Player/Music Player/ViewController.swift
@@ -15,12 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet var timelabel: UILabel!
     @IBOutlet var progressSlider: UISlider!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // dispose of any resources that can be recreated.
+    }
+    
+    //method
+    @IBAction func touchUpPlayPauseButtion(_sender: UIButton) {
+        print("button tapped")
+    }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
+    
+    
 }
 
