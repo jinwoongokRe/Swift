c856b7bec6d165df5dcd349b8f6bbc47efccbea3
cbycongbab <jinwoongoknaver@gmail.com>
2022-06-02 23:05:44 +0900

    220602_02
    second do it!

--- jw_practice/SimpleTable/SimpleTable/TableViewController.swift
+++ jw_practice/SimpleTable/SimpleTable/TableViewController.swift
@@ -25,7 +25,12 @@ class TableViewController: UITableViewController {
          self.navigationItem.leftBarButtonItem = self.editButtonItem
         //오른쪽 화면에는 이미 Add 버튼이 있으니 edit 버튼을 왼쪽에 추가함 >> right를 left로 수정
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tvListView.reloadData() //이 함수는 뷰가 전환될 때 호출되는 함수,
+        //리스트가 추가되어 main view로 돌아올 때 호출되며 추가된 내용을 리스트에 보여준다.
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -79,7 +84,16 @@ class TableViewController: UITableViewController {
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
         //하나의 목록을 선택하여 다른 곳으로 이동하는 소스를 추가하기
-
+        //먼저 변수 만들어서 이동할 변수를 기억후 이동할 목록을 삭제하고 변수에 저장된 내용을 이동한 곳으로 삽입.
+        let itemToMove = items[(fromIndexPath as NSIndexPath).row] // 이동할 아이템 저장
+        let itemImageToMove = items[(fromIndexPath as NSIndexPath).row]//이동할 이미지 저장
+        items.remove(at: (fromIndexPath as NSIndexPath).row) //이동할 아이템 삭제
+        itemsImageFile.remove(at: (fromIndexPath as NSIndexPath).row)
+        //이동할 이미지 삭제 그리고 그 뒤 이미지들의 인덱스 재정렬
+        items.insert(itemToMove, at: (to as NSIndexPath).row)
+        //삭제된 아이템을 이동할 위치로 삽입, 삽입한 아이템들의 인덱스가 재정렬됨.
+        itemsImageFile.insert(itemImageToMove, at: (to as NSIndexPath).row)
+        //삭제된 아이템의 이미지를 이동할 위치로 삽입함. 또한 삽입한 아이템 이미지 뒤의 이미지들 인덱스 재정렬됨.
     }
     
 
